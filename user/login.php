900da84a086545866840f337e1294ce383fcdb7e
ana <anaali0709@gmail.com>
2025-10-24 13:39:39 -0300

    TCC_Atualizado

---
+++ user/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="user.css">
+</head>
+<body>
+
+<div class="circle">
+        <img class="imgSign" src="userImg/user1.png" alt="">
+        <div class="contanerBranco">
+            <h2 class="h2">Sign in</h2>
+                <div class="icons-container">
+                    <img class="icons" src="userImg/facebook.png" alt="">
+                    <img class="icons" src="userImg/google.png" alt="">
+                    <img class="icons" src="userImg/linkedin.png" alt="">
+                </div>
+            
+        </div>
+</div>
+
+</body>
+</html>
